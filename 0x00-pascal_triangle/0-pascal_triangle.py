e73b92e8ceecdb173a91bd943bffc4fafe239029
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-08 01:03:42 +0200

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+pascal triangle definintion
+"""
+def pascal_triangle(n):
+    """pascal triangle method to solve
+
+    Args:
+        n (int): number of rows in 
+    """
+    if n <= 0:
+        return []
+    if type(n) is not int:
+        raise TypeError("n must be a positive integer")
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            
+            triangle.append([1])
+        elif i == 1:
+            triangle.append([1, 1])
+        else:
+            triangle.append([])
+            for j in range(i+1):
+                if j == 0:
+                    triangle[i].append(1)
+                elif j == i:
+                    triangle[i].append(1)
+                else:
+                    triangle[i].append(triangle[i - 1][j] + triangle[i - 1][j -1])
+        
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+# repo to solve pascal triangle
+# every item is the sum of the 2 items right above it and we have a 1 at the begining and at the end of the array
