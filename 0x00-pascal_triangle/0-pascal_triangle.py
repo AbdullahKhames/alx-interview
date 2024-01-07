ef4df4d2f8a8db37d8d43bd937ea592275a7cd52
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-08 01:13:19 +0200

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 pascal triangle definintion
 """
+
+
 def pascal_triangle(n):
     """pascal triangle method to solve
 
@@ -15,18 +17,17 @@ def pascal_triangle(n):
     triangle = []
     for i in range(n):
         if i == 0:
-            
             triangle.append([1])
         elif i == 1:
             triangle.append([1, 1])
         else:
             triangle.append([])
-            for j in range(i+1):
+            for j in range(i + 1):
                 if j == 0:
                     triangle[i].append(1)
                 elif j == i:
                     triangle[i].append(1)
                 else:
-                    triangle[i].append(triangle[i - 1][j] + triangle[i - 1][j -1])
-        
+                    triangle[i].append(triangle[i - 1][j] +
+                    triangle[i - 1][j -1])
     return triangle
