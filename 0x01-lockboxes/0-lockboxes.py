2f5107ca153ecf2d2232a12e67a15c61713ef9c8
AbdullahKhames <abdallahyouniiss@gmail.com>
2024-01-11 09:02:36 +0200

    try 1 brute force

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -24,14 +24,3 @@ def canUnlockAll(boxes):
             keys.update(boxes[i])
 
     return boxes_count == len(keys)
-
-
-if __name__ == '__main__':
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))
