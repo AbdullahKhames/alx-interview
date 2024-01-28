c80f919c755083329cb38c92cc8e703ed3ef88a6
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-28 20:45:26 +0200

    validate input

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,11 @@
 """module to parse logs
 """
 import re
+import sys
+
+
+sizes = [0]
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
 def validate_format(log):
@@ -16,51 +21,30 @@ def validate_format(log):
     return bool(re.match(pattern, log))
 
 
-def print_details(file_size, details):
-    """function to print details
-
-    Args:
-        file_size: the size of the files
-        details (array of codes): array to be sorted
-    """
-    print(f'File size: {file_size}')
-    for k, v in details.items():
-        if v is not None and v != 0:
-            print(f"{k}: {v}")
-
-
 def parse_logs():
-    """function to parse logs
-    """
-    pattern = r'"GET /projects/260 HTTP/1.1" (\d+) (\d+)'
+    print('File size: {}'.format(sum(sizes)))
+    for s_code, count in sorted(codes.items()):
+        if count:
+            print('{}: {}'.format(s_code, count))
 
-    log = input()
 
-    counter = 0
-    file_size = 0
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
-    while log:
+try:
+    for i, line in enumerate(sys.stdin, start=1):
+        matches = line.rstrip().split()
         try:
-            if not validate_format(log):
-                log = input()
-                continue
-            # Use re.search to find the match in the line
-            match = re.search(pattern, log)
-            # Check if a match is found
-            if match:
-                status_code = int(match.group(1))
-                if status_code in codes:
-                    codes[status_code] += 1
-                file_size += int(match.group(2))
-            counter += 1
-
-            if counter % 10 == 0:
-                print_details(file_size, codes)
-            log = input()
-        except KeyboardInterrupt as ex:
-            print_details(file_size, codes)
-            continue
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in codes.keys():
+                codes[status_code] += 1
+            sizes.append(int(file_size))
+        except Exception:
+            pass
+        if i % 10 == 0:
+            parse_logs()
+    parse_logs()
+except KeyboardInterrupt:
+    parse_logs()
+    raise
 
 
 if __name__ == '__main__':
