07906973c243a53850237aec42367e4dc4a3156e
AbdullahKhames <abdullahkhames96@gmail.com>
2024-03-01 00:03:23 +0200

    solving make change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,15 +11,32 @@ def makeChange(coins, total):
     Returns:
         int: the count of coins
     """
+    # if total <= 0:
+    #     return 0
+    # coins.sort(reverse=True)
+    # current_sum = []
+    # for i in range(len(coins)):
+    #     while sum(current_sum) + coins[i] <= total:
+    #         current_sum.append(coins[i])
+    #     i += 1
+    # if sum(current_sum) == total:
+    #     return len(current_sum)
+    # else:
+    #     return -1
     if total <= 0:
         return 0
+
     coins.sort(reverse=True)
-    current_sum = []
-    for i in range(len(coins)):
-        while sum(current_sum) + coins[i] <= total:
-            current_sum.append(coins[i])
-        i += 1
-    if sum(current_sum) == total:
-        return len(current_sum)
-    else:
+
+    count = 0
+    for coin in coins:
+        if total:
+            if total % coin == 0:
+                return count + total // coin
+            count += total // coin
+            total = total % coin
+
+    if total != 0:
         return -1
+
+    return count
