25b440a5ea679d5359a29930ab63a53143dd289e
AbdullahKhames <abdullahkhames96@gmail.com>
2024-03-07 14:00:49 +0200

    inital commit

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,7 +4,7 @@
 
 def island_perimeter(grid):
     """method to solve island perimeter"""
-    R,C = len(grid), len(grid[0])
+    R, C = len(grid), len(grid[0])
     perimeter = 0
     # Traverse the grid
     for i in range(R):
@@ -15,18 +15,18 @@ def island_perimeter(grid):
                 # Check whether top neighbour is a land and increment it by 1
                 # as it intersects
                 if i - 1 >= 0 and grid[i-1][j] == 1:
-                    count += 1 
+                    count += 1
                 # Check whether left neighbour is a land and increment it by 1
                 # as it intersects
                 if j - 1 >= 0 and grid[i][j-1] == 1:
-                     count += 1
+                    count += 1
                 # Check whether bot neighbour is a land and increment it by 1
                 # as it intersects
                 if i + 1 < R and grid[i + 1][j] == 1:
-                    count += 1 
+                    count += 1
                 # Check whether right neighbour is a land and increment it by 1
                 # as it intersects
                 if j + 1 < C and grid[i][j + 1] == 1:
-                     count += 1
+                    count += 1
                 perimeter += 4 - count
     return perimeter
